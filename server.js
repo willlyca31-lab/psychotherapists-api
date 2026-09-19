462080b555a825a192167df48185bd2cd31eb8a4
wilsonews <willlyca31@gmail.com>
2026-09-18 18:33:21 -0600

    Initialize psychotherapists API with Express

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
