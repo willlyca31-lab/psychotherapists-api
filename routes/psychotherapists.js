5ee212ec288b0739a53464788478615743b39467
wilsonews <willlyca31@gmail.com>
2026-09-23 09:19:42 -0600

    Add psychotherapist PUT and DELETE routes

--- routes/psychotherapists.js
+++ routes/psychotherapists.js
@@ -5,16 +5,29 @@ const router = express.Router();
 const {
   getAllPsychotherapists,
   getPsychotherapistById,
-  createPsychotherapist
+  createPsychotherapist,
+  updatePsychotherapist,
+  deletePsychotherapist
 } = require("../controllers/psychotherapists");
 
 // GET all psychotherapists
+// Example: GET /psychotherapists
 router.get("/", getAllPsychotherapists);
 
 // GET one psychotherapist by ID
+// Example: GET /psychotherapists/by-id?id=1884858585858
 router.get("/by-id", getPsychotherapistById);
 
 // POST - Create a new psychotherapist
+// Example: POST /psychotherapists
 router.post("/", createPsychotherapist);
+
+// PUT - Update a psychotherapist
+// Example: PUT /psychotherapists/1884858585858
+router.put("/:id", updatePsychotherapist);
+
+// DELETE - Delete a psychotherapist
+// Example: DELETE /psychotherapists/1884858585858
+router.delete("/:id", deletePsychotherapist);
 
 module.exports = router;
